cbcc433aae7fc31f6f7d26cfc5f19c8315224df7
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-10-08 19:02:21 +0700

    upddate db

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
-
 const port = 3000;
 
+app.use(express.json());
 // Impor rute pelanggan
 const customerRoutes = require("./router/customerRoutes"); // Pastikan jalur ini benar
+app.use("/api/v1/customers", customerRoutes);
+// Routes
+app.use(customerRoutes);
 
-app.use(express.json());
-app.use("/api/v1/customers", customerRoutes); // Ganti ke "/api/v1/customers"
-
+// Middleware untuk menangani rute yang tidak ditemukan
 app.use((req, res, next) => {
   res.status(404).json({
     status: "404",
-    message: "Api Not Exist",
+    message: "API Not Exist",
   });
 });
 
--- controller/customerController.js
+++ controller/customerController.js
@@ -1,4 +1,5 @@
-const { Car } = require("../models");
+const { Customer } = require("../models");
+const customer = require("../models/customer");
 
 async function getCustomer(req, res) {
   try {
@@ -21,80 +22,116 @@ async function getCustomer(req, res) {
 }
 
 async function getCustomerById(req, res) {
-  //const id = req.params.id;
+  const id = req.params.id;
   try {
-    const id = req.params.id;
-    const customer = await Customer.findById(id);
+    const customer = await Customer.findByPk(id);
+
+    if (!customer) {
+      return res.status(404).json({
+        status: "404",
+        message: "Car Not Found!",
+      });
+    }
+
     res.status(200).json({
-      status: "success",
-      data: {
-        customer,
-      },
+      status: "200",
+      message: "Success get cars data",
+      isSuccess: true,
+      data: customer,
     });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
+  } catch (error) {
+    res.status(500).json({
+      status: "500",
+      message: "Failed to get cars data",
+      isSuccess: false,
+      error: error.message,
     });
   }
 }
 async function deleteCustomer(req, res) {
+  const id = req.params.id;
   try {
-    const id = req.params.id;
-    await Customer.findByIdAndDelete(id);
-    res.status(200).json({
-      status: "success",
-      message: "berhasil delete data",
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
+    const customer = await Customer.findByPk(id);
+
+    if (car) {
+      await car.destroy();
+
+      res.status(200).json({
+        status: "200",
+        message: "Success get cars data",
+        isSuccess: true,
+        data: customer,
+      });
+    } else {
+      res.status(404).json({
+        status: "404",
+        message: "Car Not Found!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "500",
+      message: "Failed to get cars data",
+      isSuccess: false,
+      error: error.message,
     });
   }
 }
-
 async function updateCustomer(req, res) {
+  const id = req.params.id;
+  const { name, email, phone } = req.body;
+  console.log("name", name);
   try {
-    console.log("MASUK EDIT GAK");
-    const id = req.params.id;
-    const customer = await Customer.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: "success",
-      message: "berhasil update data",
-      data: {
-        customer,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
+    const customer = await Customer.findByPk(id);
+
+    if (customer) {
+      customer.name = name;
+      customer.email = email;
+      customer.phone = phone;
+
+      await customer.save();
+
+      res.status(200).json({
+        status: "200",
+        message: "Success get cars data",
+        isSuccess: true,
+        data: customer,
+      });
+    } else {
+      res.status(404).json({
+        status: "404",
+        message: "Car Not Found!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "500",
+      message: "Failed to get cars data",
+      isSuccess: false,
+      error: error.message,
     });
   }
 }
 
 async function createCustomer(req, res) {
+  const { name, email, phone } = req.body;
   try {
-    const newCustomer = await Customer.create(req.body);
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        customer: newCustomer,
-      },
+    const newCustomer = await Customer.create({ name, email, phone });
+    res.status(200).json({
+      status: "Success",
+      message: "Ping successfully",
+      isSuccess: true,
+      data: newCustomer,
     });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
+  } catch (error) {
+    res.status(500).json({
+      status: "500",
+      message: "Failed to get cars data",
+      isSuccess: false,
+      error: error.message,
     });
   }
 }
-
 module.exports = {
   createCustomer,
   getCustomer,
